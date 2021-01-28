108dab6b396fa12a6dd21281f3fbf39f091b2384
Cyrmax_at_home <belousov.k.m@ya.ru>
2021-01-28 11:04:36 +0300

    Fixed builddoc.py to include russian and exclude other unneeded languages

--- builddoc.py
+++ builddoc.py
@@ -17,17 +17,7 @@ def build(dest="."):
     except OSError:
         pass
 
-    for lang in ("es", "it"):
-        p = join(SRC, lang, "htm")
-        dp = join(DEST, lang)
-        try:
-            os.mkdir(dp)
-        except OSError:
-            pass
-        for n in os.listdir(p):
-            shutil.copyfile(join(p, n), join(dp, n))
-
-    for lang in ("en", "pt-BR"):
+    for lang in ("en", "ru"):
         p = join(SRC, lang)
         dp = join(DEST, lang)
         open(join(p, "stats.inc"), "w").write(rules2doc.stats)
@@ -42,10 +32,6 @@ def build(dest="."):
                     writer_name="html",
                     destination_path=join(dp, n[:-3] + "htm"),
                 )
-
-    shutil.copyfile(join(DEST, "en/units.htm"), join(DEST, "it/units.htm"))
-    for n in ("mapmaking", "modding", "aimaking"):
-        shutil.copyfile(join(DEST, "en/%s.htm" % n), join(DEST, "pt-BR/%s.htm" % n))
 
 
 if __name__ == "__main__":
